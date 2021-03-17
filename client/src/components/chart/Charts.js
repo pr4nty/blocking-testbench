a039f638572ce9b5ac260c74100be5ef05de5ac6
pranty <pranty@hotmail.de>
2021-03-17 11:21:29 +0100

    Scatter Diagram fix Nr3

--- client/src/components/chart/Charts.js
+++ client/src/components/chart/Charts.js
@@ -38,44 +38,42 @@ const Charts = ( record ) => {
     const indexOfMaxValue = zAxis.indexOf(Math.max(...zAxis));
 
     return (
-      <div>
-        {/* Line Chart */}
-        <div className="row">
-          <div className="column">
-            <h2>Deviation along the Z axis</h2>
-              <LineChart width={500} height={300} data={data.testData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="time" />
-                <YAxis dataKey="zAxis" />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="zAxis" stroke="#82ca9d" />
-              </LineChart>        
+        <div>
+          {/* Line Chart */}
+          <div className="row">
+            <div className="column">
+              <h2>Deviation along the Z axis</h2>
+                <LineChart width={500} height={300} data={data.testData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="time" />
+                  <YAxis dataKey="zAxis" />
+                  <Tooltip />
+                  <Legend />
+                  <Line type="monotone" dataKey="zAxis" stroke="#82ca9d" />
+                </LineChart>        
+              </div>
+            <div className="column">
+              <h2>Key Figures</h2>
+              <p>Maximum deflection in Z : {Math.max(...zAxis)} N</p>
+              <p>Time to max deflection in Z : {time[indexOfMaxValue]} s</p>
             </div>
-          <div className="column">
-            <h2>Key Figures</h2>
-            <p>Maximum deflection in Z : {Math.max(...zAxis)} N</p>
-            <p>Time to max deflection in Z : {time[indexOfMaxValue]} s</p>
+          </div>
+          {/* Scatter Chart */}
+          <div className="row">
+              <div className="column">
+                <h2>Scatter Chart X and Y axis</h2>
+                <ScatterChart width={500} height={500} margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="xAxis" name="X" unit=" N" type="number" domain={['auto', 'auto']}/>
+                  <YAxis dataKey="yAxis" name="Y" unit=" N" type="number" domain={['auto', 'auto']}/>
+                  <ZAxis dataKey="zAxis" name="Z" unit=" N" range={[10, 150]}/>
+                  <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                  <Legend />
+                  <Scatter name="X/Y Scatter intesity" data={data.testData} fill="#8884d8" shape="cross"/>
+                </ScatterChart>
+              </div>
           </div>
         </div>
-        {/* Scatter Chart */}
-        <div className="row">
-          <div className="column">
-            <h2>Scatter Chart X and Y axis</h2>
-              <ScatterChart width={730} height={250} margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="xAxis" name="X axis" unit="N" />
-                <YAxis dataKey="yAxis" name="Y Axis" unit="N" />
-                <ZAxis dataKey="time"  name="time" unit="s" />
-                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                <Scatter name="X and Y scatter" data={data.testData} fill="#8884d8" />
-              </ScatterChart>
-          </div>
-          <div className="column">
-            <h2>Key Figures</h2>
-          </div>
-        </div>
-      </div>
       );
 }
 
